1142cf0b49b0ac1b25195604b22bed511516b5bd
DrewHouchens13 <drewhouchens@icloud.com>
2025-10-06 23:21:24 -0600

    Change to Fix CSS again

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
 ]
-
-# Serve static files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    # Also serve static files without proxy prefix for local development
-    urlpatterns += static('/static/', document_root=settings.STATIC_ROOT)
