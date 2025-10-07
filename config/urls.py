2d0dea5a270dbe11b9ae8d0748e2b2cb3ffb4355
DrewHouchens13 <drewhouchens@icloud.com>
2025-10-06 23:25:34 -0600

    More changes for proxy prefix

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.contrib.staticfiles.views import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
 ]
+
+# In development, serve static files at /static/ (what Django receives after proxy strips /proxy/8000)
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve),
+    ]
